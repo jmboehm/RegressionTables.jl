5b9bc02fd69e9f31d1016d3a5bcaf598cb497822
Fabian Greimel <fabgrei@gmail.com>
2020-07-07 11:21:00 +0200

    extend transform labels

--- src/label_transforms/default_transforms.jl
+++ src/label_transforms/default_transforms.jl
@@ -1,8 +1,26 @@
-# this function escapes the ampersand, which is probably the most common use case
-function escape_ampersand(s::String)
-    repl_dict = Dict("&" => "\\&") 
-    for (old, new) in repl_dict
-        s = replace.(s, Ref(old => new))
+function _escape(repl_dict::AbstractDict)
+    function escape(s::String)  
+        for (old, new) in repl_dict
+            s = replace.(s, Ref(old => new))
+        end
+        return s
     end
-    return s
 end
+
+escape_latex_dict = Dict("&" => "\\&", "%" => "\\%", "\$" => "\\\$", "#" => "\\#", "_" => "\\_", "{" => "\\{", "}" => "\\}")
+
+function _escape(s::Symbol)
+  if s == :ampersand
+    _escape(Dict("&" => "\\&"))
+  elseif s == :underscore
+    _escape(Dict("_" => "\\_"))
+  elseif s == :underscore2space
+    _escape(Dict("_" => " "))  
+  elseif s == :latex
+    _escape(escape_latex_dict)
+  else
+    @error "Please provide `:ampersand`, `:underscore`, `:underscore2space`, `:latex`, a `::Dict` or a custom function."
+  end
+end
+
+
